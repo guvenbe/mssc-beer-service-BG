c23e7c06fcd8629f61477a56d729ecc648594324
Bora Guven <guvenbe@gmail.com>
2020-07-12 22:00:11 -0400

    initial project add Closes #1

---
+++ src/main/java/guru/springframework/msscbeerservicebg/MsscBeerServiceBgApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.msscbeerservicebg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscBeerServiceBgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsscBeerServiceBgApplication.class, args);
+    }
+
+}
