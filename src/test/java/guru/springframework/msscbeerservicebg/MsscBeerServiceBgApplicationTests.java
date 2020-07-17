9f93d283f22ba913a1ef5c867f141859a414aeb2
Bora Guven <guvenbe@gmail.com>
2020-07-17 01:49:32 -0400

    Adding circle CI

--- src/test/java/guru/springframework/msscbeerservicebg/MsscBeerServiceBgApplicationTests.java
+++ src/test/java/guru/springframework/msscbeerservicebg/MsscBeerServiceBgApplicationTests.java
@@ -1,4 +1,4 @@
-package guru.springframework.msscbeerserviceb;
+package guru.springframework.msscbeerservicebg;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
