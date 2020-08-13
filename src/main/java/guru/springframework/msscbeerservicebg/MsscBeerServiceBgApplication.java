e587347234a1a8cd5309b5908c743ea0787c7d93
Bora Guven <guvenbe@gmail.com>
2020-08-12 21:05:48 -0400

    sql and properties

--- src/main/java/guru/springframework/msscbeerservicebg/MsscBeerServiceBgApplication.java
+++ src/main/java/guru/springframework/msscbeerservicebg/MsscBeerServiceBgApplication.java
@@ -2,8 +2,9 @@ package guru.springframework.msscbeerservicebg;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jms.artemis.ArtemisAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = ArtemisAutoConfiguration.class)
 public class MsscBeerServiceBgApplication {
 
     public static void main(String[] args) {
