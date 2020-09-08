c68ad83a4876dfb6e528f2644e6f8d7a0f2e94e2
Bora Guven <guvenbe@gmail.com>
2020-09-07 21:37:58 -0400

    add Hystrix Circuit Breaker for Inventory Service Closes #25

---
+++ src/main/java/guru/springframework/msscbeerservicebg/services/inventory/InventoryServiceFeignClientFailover.java
@@ -0,0 +1,20 @@
+package guru.springframework.msscbeerservicebg.services.inventory;
+
+import guru.springframework.msscbeerservicebg.services.inventory.model.BeerInventoryDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.UUID;
+@RequiredArgsConstructor
+@Component
+public class InventoryServiceFeignClientFailover implements InventoryServiceFeignClient {
+
+    private final InventoryFailoverFeignClient failoverFeignClient;
+
+    @Override
+    public ResponseEntity<List<BeerInventoryDto>> getOnhandInventory(UUID beerId) {
+        return failoverFeignClient.getOnhandInventory();
+    }
+}
